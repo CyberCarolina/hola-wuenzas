bce81cc1a1182ad7b2cc63fe4cc7b8b181dab36a
CyberCarolina <71851673+CyberCarolina@users.noreply.github.com>
2020-09-24 17:37:48 -0500

    xd
    xd

---
+++ proyecto/fundamentos de java V=/secion 1.js
@@ -0,0 +1,10 @@
+var piloto
+piloto = "uwu "
+var nombre
+nombre = "soy alejandro"
+var edad
+edad = ", tengo 12 años :3,"
+var datos
+datos = " yo hago nada y soy estudiante de la escuala Tec. Num. 26 y mis gustos son: el chocolate y las chamaras uwu "
+console.log ('hola ' + piloto + nombre + edad + datos )
+document
---
+++ proyecto/js dekiser/app.js
@@ -0,0 +1,37 @@
+/*document.write("<h1>HOLA AMIGOS DE DEKIS </h1>");
+
+const PI = 3.1416;
+
+PI = 100;
+
+console.log(PI)
+*/
+
+var numeroUno = 100;
+
+var numeroDos = 200;
+
+var resultado = numeroUno <= numeroDos;
+
+
+
+var contraseña = ':V'
+
+var entrada = ':v'
+
+var hola = contraseña == entrada;
+
+if (hola == true){
+    console.log('login Correcto ');
+}
+
+
+
+
+else  {
+    console.log('login Incorrecto');
+}
+
+
+
+console.log(hola);
